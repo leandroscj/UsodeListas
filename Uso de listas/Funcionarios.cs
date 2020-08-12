6a3da616217ce785445428afefe5aa966d6ff190
Leandro Sousa <leandroscjTI@gmail.com>
2020-08-12 13:06:16 -0300

    Add project files.

---
+++ Uso de listas/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿namespace Uso_de_listas
+{
+    public class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void Acrescentar(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2");
+        }
+    }
+}
---
+++ Uso de listas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Uso_de_listas;
+
+namespace Usodelistas
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Quantos dados serão inseridos?");
+            var qtd_de_dados = int.Parse(Console.ReadLine());
+            List<Funcionarios> funcionario = new List<Funcionarios>();
+
+            for (int i = 0; i < qtd_de_dados; i++)
+            {
+                Console.Write("Id= ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome= ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário= ");
+                double salario = double.Parse(Console.ReadLine());
+                funcionario.Add(new Funcionarios(id, nome, salario));
+            }
+
+            Console.Write("Qual o id do funcionario? ");
+            int digitado = int.Parse(Console.ReadLine());
+
+            Funcionarios find = funcionario.Find(x => x.Id == digitado);
+
+            if (find.Id != null)
+            {
+                Console.Write("Qual a porcentagem? ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                find.Acrescentar(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Este id não existe!");
+            }
+            foreach (Funcionarios obj in funcionario)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
