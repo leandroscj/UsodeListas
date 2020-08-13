4d1c05bc25c682c5f144f8fc4a70b2c71e0e1b79
Leandro Sousa <leandroscjTI@gmail.com>
2020-08-13 19:32:19 -0300

    Edição no texto exibido para o usuario

--- Uso de listas/Program.cs
+++ Uso de listas/Program.cs
@@ -8,35 +8,35 @@ namespace Usodelistas
     {
         private static void Main(string[] args)
         {
-            Console.Write("Quantos dados serão inseridos?");
+            Console.Write("Quantos dados serão inseridos? ");
             var qtd_de_dados = int.Parse(Console.ReadLine());
             List<Funcionarios> funcionario = new List<Funcionarios>();
 
             for (int i = 0; i < qtd_de_dados; i++)
             {
-                Console.Write("Id= ");
+                Console.Write("ID: ");
                 int id = int.Parse(Console.ReadLine());
-                Console.Write("Nome= ");
+                Console.Write("Nome: ");
                 string nome = Console.ReadLine();
-                Console.Write("Salário= ");
+                Console.Write("Salário: ");
                 double salario = double.Parse(Console.ReadLine());
                 funcionario.Add(new Funcionarios(id, nome, salario));
             }
 
-            Console.Write("Qual o id do funcionario? ");
+            Console.Write("Qual o ID do funcionario que vai ser aumentado o salário? ");
             int digitado = int.Parse(Console.ReadLine());
 
             Funcionarios find = funcionario.Find(x => x.Id == digitado);
 
             if (find != null)
             {
-                Console.Write("Qual a porcentagem? ");
+                Console.Write("Qual a porcentagem do aumento? ");
                 double porcentagem = double.Parse(Console.ReadLine());
                 find.Acrescentar(porcentagem);
             }
             else
             {
-                Console.WriteLine("Este id não existe!");
+                Console.WriteLine("Este ID não existe!");
             }
             foreach (Funcionarios obj in funcionario)
             {
